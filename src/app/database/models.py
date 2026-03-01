2af14f16cafc1991004bddde8516605b5ad2a300
Qu5antum <erae88959@gmail.com>
2026-03-01 18:48:42 +0300

    user model

---
+++ src/app/database/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+from src.app.database.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True)
+
+
+
+
